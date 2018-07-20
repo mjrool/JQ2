79cef06d236a9def29973bc92841b6cb8c4de24a
MariaJose <mjroolma@gmail.com>
2018-07-19 20:50:30 -0400

    2do punto

--- ACTP2_jQueryEventos-master/assets/js/script.js
+++ ACTP2_jQueryEventos-master/assets/js/script.js
@@ -6,6 +6,25 @@ $(window).on('load', function(){
 			alert("pagina cargada")
 		});
 
+$('.card').click(function(e){
+	 e.preventDefault();
+    $(this).toggleClass('.card--open');
+   
+});
+
+$('.card').click('a', function(e){
+
+	e.preventDefault();
+	e.stopPropagation();
+
+})
+
+// $(".card__highlight").click(function(e){
+//     alert($(this).attr('alt'));
+// });
+
+
+
 });
 
 
